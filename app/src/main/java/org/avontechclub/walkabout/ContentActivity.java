9cd36bf7128b274d27c01133ccadb8e40d6be06e
Jon <jonkarkour@hotmail.com>
2016-03-12 20:33:45 -0500

    BEST ARROW EVER

--- app/src/main/java/org/avontechclub/walkabout/ContentActivity.java
+++ app/src/main/java/org/avontechclub/walkabout/ContentActivity.java
@@ -4,11 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class ContentActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +29,7 @@ public class ContentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_content);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,8 +40,9 @@ public class ContentActivity extends AppCompatActivity {
         });
     }
 
-    public void sendHome(View view) {
+
+    /*public void sendHome(View view) {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
-    }
+    }*/
 }
