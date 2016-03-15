9fb39ad33b3b0d26c1ed2e78bc73a1ba2eec2057
swseighman <scottseighman@gmail.com>
2016-03-15 15:51:07 -0400

    Testing Git from Scott

---
+++ app/src/main/java/org/avontechclub/walkabout/ContentActivity.java
@@ -0,0 +1,126 @@
+package org.avontechclub.walkabout;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ContentActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        Intent intent = getIntent();
+        String locationID = intent.getStringExtra(HomeActivity.LOC_ID);
+
+        int titleID = getResources().getIdentifier("title_" + locationID, "string", "org.avontechclub.walkabout");
+        int imageID = getResources().getIdentifier("image_" + locationID, "drawable", "org.avontechclub.walkabout");
+
+        String title = getString(titleID);
+        getSupportActionBar().setTitle(title);
+
+        ImageView imageView = (ImageView)findViewById(R.id.actionbar_image);
+        imageView.setImageResource(imageID);
+
+        TextView textView = (TextView) findViewById(R.id.content);
+        AssetManager assetManager = getResources().getAssets();
+        InputStream inputStream;
+        String locationContent = "";
+        try {
+            inputStream = assetManager.open(locationID + ".txt");
+            locationContent = convert_to_String(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        textView.setText(locationContent);
+        Bitmap image = BitmapFactory.decodeResource(getResources(), imageID);
+        Palette p = Palette.from(image).generate();
+        int color = 1;
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        collapsingToolbarLayout.setContentScrimColor(p.getMutedColor(color));
+        Window window = getWindow();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1){
+            window.setStatusBarColor(p.getDarkMutedColor(color));
+        }
+
+
+        //window.setStatusBarColor(p.getDarkMutedColor(color));
+
+    }
+    public String convert_to_String(InputStream inputStream)
+    {
+
+        BufferedReader bufferedReader=null;
+        StringBuilder stringBuilder=new StringBuilder();
+        String line;
+        try{
+            bufferedReader= new BufferedReader(new InputStreamReader(inputStream));
+            while ((line=bufferedReader.readLine())!=null)
+            {    stringBuilder.append("\n");
+                stringBuilder.append(line);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return  stringBuilder.toString();
+
+    }
+
+
+
+}
---
+++ app/src/main/java/org/avontechclub/walkabout/HomeActivity.java
@@ -0,0 +1,167 @@
+package org.avontechclub.walkabout;
+
+import android.content.Intent;
+import android.net.Uri;
+//import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.os.Bundle;
+//import android.os.Parcelable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.support.v7.widget.CardView;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class HomeActivity extends AppCompatActivity {
+    public final static String LOC_ID = "org.avontechclub.walkabout.LOCATION_ID";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+    }
+
+    public void onResume() {
+        super.onResume();
+        //Intent intent = new Intent();
+        //android.nfc.NdefMessage[] msgs;
+        String id;
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            id = NfcAdapter.EXTRA_ID;
+            TextView textView = (TextView) findViewById(R.id.nfctext);
+            textView.setText(id);
+
+            /*Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMsgs != null){
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++){
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }*/
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void sendContent(View view) {
+
+        CardView cardView = new CardView(this);
+        cardView.setCardElevation(14.5f);
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        );
+        cardView.setLayoutParams(layoutParams);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.cardHolder);
+        layout.addView(cardView);
+
+        Intent intent = new Intent(this, ContentActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String locationID = editText.getText().toString();
+        intent.putExtra(LOC_ID, locationID);
+        startActivity(intent);
+    }
+
+    public void sendLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+
+
+        startActivity(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Home Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://org.avontechclub.walkabout/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Home Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://org.avontechclub.walkabout/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
